1b0044042a0e827fc4638de7cfb9d33cc286b037
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2021-12-15 02:50:32 +0200

    Add files via upload

---
+++ VRP_Sugar/Solver.py
@@ -0,0 +1,231 @@
+from VRP_Model import *
+from SolutionDrawer import *
+
+class Solution:
+    def __init__(self):
+        self.cost = 0.0
+        self.routes = []
+
+class RelocationMove(object):
+    def __init__(self):
+        self.originRoutePosition = None
+        self.targetRoutePosition = None
+        self.originNodePosition = None
+        self.targetNodePosition = None
+        self.costChangeOriginRt = None
+        self.costChangeTargetRt = None
+        self.moveCost = None
+
+    def Initialize(self):
+        self.originRoutePosition = None
+        self.targetRoutePosition = None
+        self.originNodePosition = None
+        self.targetNodePosition = None
+        self.costChangeOriginRt = None
+        self.costChangeTargetRt = None
+        self.moveCost = 10 ** 9
+
+
+class SwapMove(object):
+    def __init__(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.costChangeFirstRt = None
+        self.costChangeSecondRt = None
+        self.moveCost = None
+    def Initialize(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.costChangeFirstRt = None
+        self.costChangeSecondRt = None
+        self.moveCost = 10 ** 9
+
+
+class CustomerInsertion(object):
+    def __init__(self):
+        self.customer = None
+        self.route = None
+        self.cost = 10 ** 9
+
+class CustomerInsertionAllPositions(object):
+    def __init__(self):
+        self.customer = None
+        self.route = None
+        self.insertionPosition = None
+        self.cost = 10 ** 9
+
+class TwoOptMove(object):
+    def __init__(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.moveCost = None
+    def Initialize(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.moveCost = 10 ** 9
+
+class Solver:
+    def __init__(self, m):
+        self.allNodes = m.allNodes
+        self.customers = m.customers
+        self.depot = m.allNodes[0]
+        self.distanceMatrix = m.matrix
+        self.capacity = m.capacity
+        self.sol = None
+        self.bestSolution = None
+
+    def solve(self):
+        self.SetRoutedFlagToFalseForAllCustomers()
+        self.MinimumInsertions()
+        self.ReportSolution(self.sol)
+        return self.sol
+
+    def SetRoutedFlagToFalseForAllCustomers(self):
+        for i in range(0, len(self.customers)):
+            self.customers[i].isRouted = False
+
+    def Always_keep_an_empty_route(self):
+        if len(self.sol.routes) == 0:
+            rt = Route(self.depot, self.capacity)
+            self.sol.routes.append(rt)
+        else:
+            rt = self.sol.routes[-1]
+            if len(rt.sequenceOfNodes) > 2:
+                rt = Route(self.depot, self.capacity)
+                self.sol.routes.append(rt)
+                
+    def IdentifyMinimumCostInsertion(self, best_insertion):
+        for i in range(0, len(self.customers)):
+            candidateCust: Node = self.customers[i]
+            if candidateCust.isRouted is False:
+                for rt in self.sol.routes:
+                    if rt.load + candidateCust.demand <= rt.capacity:
+                        for j in range(0, len(rt.sequenceOfNodes) - 1):
+                            A = rt.sequenceOfNodes[j]
+                            B = rt.sequenceOfNodes[j + 1]
+                            costAdded = self.distanceMatrix[A.ID][candidateCust.ID] + self.distanceMatrix[candidateCust.ID][
+                                B.ID]
+                            costRemoved = self.distanceMatrix[A.ID][B.ID]
+                            trialCost = costAdded - costRemoved
+                            if trialCost < best_insertion.cost:
+                                best_insertion.customer = candidateCust
+                                best_insertion.route = rt
+                                best_insertion.insertionPosition = j
+                                best_insertion.cost = trialCost
+                    else:
+                        continue
+                
+    def MinimumInsertions(self):
+        model_is_feasible = True
+        self.sol = Solution()
+        insertions = 0
+
+        while insertions < len(self.customers):
+            best_insertion = CustomerInsertionAllPositions()
+            self.Always_keep_an_empty_route()
+            self.IdentifyMinimumCostInsertion(best_insertion)
+
+            if best_insertion.customer is not None:
+                self.ApplyCustomerInsertionAllPositions(best_insertion)
+                insertions += 1
+            else:
+                print('FeasibilityIssue')
+                model_is_feasible = False
+                break
+
+        if model_is_feasible:
+            self.TestSolution()
+
+    def ReportSolution(self, sol):
+        for i in range(0, len(sol.routes)):
+            rt = sol.routes[i]
+            for j in range (0, len(rt.sequenceOfNodes)):
+                print(rt.sequenceOfNodes[j].ID, end=' ')
+            print(rt.cost)
+        SolDrawer.draw('MinIns', self.sol, self.allNodes)
+        print(self.sol.cost)
+
+    def GetLastOpenRoute(self):
+        if len(self.sol.routes) == 0:
+            return None
+        else:
+            return self.sol.routes[-1]
+
+    def CalculateTotalCost(self, sol):
+        c = 0
+        for i in range(0, len(sol.routes)):
+            rt = sol.routes[i]
+            for j in range(0, len(rt.sequenceOfNodes) - 1):
+                a = rt.sequenceOfNodes[j]
+                b = rt.sequenceOfNodes[j + 1]
+                c += self.distanceMatrix[a.ID][b.ID]
+        return c
+
+    def UpdateRouteCostAndLoad(self, rt: Route):
+        tc = 0
+        tl = 0
+        for i in range(0, len(rt.sequenceOfNodes) - 1):
+            A = rt.sequenceOfNodes[i]
+            B = rt.sequenceOfNodes[i+1]
+            tc += self.distanceMatrix[A.ID][B.ID]
+            tl += A.demand
+        rt.load = tl
+        rt.cost = tc
+
+    def TestSolution(self):
+        totalSolCost = 0
+        for r in range (0, len(self.sol.routes)):
+            rt: Route = self.sol.routes[r]
+            rtCost = 0
+            rtLoad = 0
+            for n in range (0 , len(rt.sequenceOfNodes) - 1):
+                A = rt.sequenceOfNodes[n]
+                B = rt.sequenceOfNodes[n + 1]
+                rtCost += self.distanceMatrix[A.ID][B.ID]
+                rtLoad += A.demand
+            if abs(rtCost - rt.cost) > 0.0001:
+                print ('Route Cost problem')
+            if rtLoad != rt.load:
+                print ('Route Load problem')
+
+            totalSolCost += rt.cost
+
+        if abs(totalSolCost - self.sol.cost) > 0.0001:
+            print('Solution Cost problem')
+
+    def IdentifyBestInsertionAllPositions(self, best_insertion, rt):
+        for i in range(0, len(self.customers)):
+            candidateCust: Node = self.customers[i]
+            if candidateCust.isRouted is False:
+                if rt.load + candidateCust.demand <= rt.capacity:
+                    for j in range(0, len(rt.sequenceOfNodes) - 1):
+                        A = rt.sequenceOfNodes[j]
+                        B = rt.sequenceOfNodes[j + 1]
+                        costAdded = self.distanceMatrix[A.ID][candidateCust.ID] + self.distanceMatrix[candidateCust.ID][B.ID]
+                        costRemoved = self.distanceMatrix[A.ID][B.ID]
+                        trialCost = costAdded - costRemoved
+
+                        if trialCost < best_insertion.cost:
+                            best_insertion.customer = candidateCust
+                            best_insertion.route = rt
+                            best_insertion.cost = trialCost
+                            best_insertion.insertionPosition = j
+
+    def ApplyCustomerInsertionAllPositions(self, insertion):
+        insCustomer = insertion.customer
+        rt = insertion.route
+        insIndex = insertion.insertionPosition
+        rt.sequenceOfNodes.insert(insIndex + 1, insCustomer)
+        rt.cost += insertion.cost
+        self.sol.cost += insertion.cost
+        rt.load += insCustomer.demand
+        insCustomer.isRouted = True
+
