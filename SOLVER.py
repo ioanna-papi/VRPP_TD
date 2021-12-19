72d4b2c9e5e2cbaef04cfcc176d8178ded984019
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2021-12-19 09:29:27 +0200

    Update SOLVER.py

--- SOLVER.py
+++ SOLVER.py
@@ -4,3 +4,73 @@ class Solution:
     def __init__(self):
         self.dist = 0.0
         self.routes = []
+        
+class RelocationMove(object):
+    def __init__(self):
+        self.originRoutePosition = None
+        self.targetRoutePosition = None
+        self.originNodePosition = None
+        self.targetNodePosition = None
+        self.costChangeOriginRt = None
+        self.costChangeTargetRt = None
+        self.moveCost = None
+
+    def Initialize(self):
+        self.originRoutePosition = None
+        self.targetRoutePosition = None
+        self.originNodePosition = None
+        self.targetNodePosition = None
+        self.costChangeOriginRt = None
+        self.costChangeTargetRt = None
+        self.moveCost = 10 ** 9
+
+        
+class SwapMove(object):
+    def __init__(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.costChangeFirstRt = None
+        self.costChangeSecondRt = None
+        self.moveCost = None
+    def Initialize(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.costChangeFirstRt = None
+        self.costChangeSecondRt = None
+        self.moveCost = 10 ** 9
+
+
+class CustomerInsertion(object):
+    def __init__(self):
+        self.customer = None
+        self.route = None
+        self.cost = 10 ** 9
+
+class CustomerInsertionAllPositions(object):
+    def __init__(self):
+        self.customer = None
+        self.route = None
+        self.insertionPosition = None
+        self.cost = 10 ** 9
+
+class TwoOptMove(object):
+    def __init__(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.moveCost = None
+    def Initialize(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.moveCost = 10 ** 9
+
+class Solver:
+    
+    
