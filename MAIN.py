b21038ff1a3e4334d63bff03fd73d17b646a084e
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2022-01-04 22:41:33 +0200

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,9 +1,9 @@
-from SOLVER import *
+# from SOLVER import *
 
 m = Model()
 m.BuildModel()
-s = Solver(m)
-print("Best Node")
-solution = s.ApplyBestNodeMethod()
-print(s.objective(solution))
-print()
+#s = Solver(m)
+#print("Best Node")
+#solution = s.ApplyBestNodeMethod()
+#print(s.objective(solution))
+#print()
