ba231e52f62f682f15eeb437884765e21e644669
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2022-01-04 22:58:12 +0200

    Update SOLVER.py

--- SOLVER.py
+++ SOLVER.py
@@ -64,7 +64,7 @@ class Solver:
     def __init__(self, m):
         self.allNodes = m.allNodes
         self.customers = m.customers
-        self.depot = m.allNodes[:1]
+        self.depot = m.allNodes[0]
         self.distanceMatrix = m.matrix
         self.total_route_time = 0
         self.total_route_profit = 0
