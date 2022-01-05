ffb9729ae383ff7b768286ceb2babc50e4953674
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2022-01-05 10:01:49 +0200

    Update SOLVER.py

--- SOLVER.py
+++ SOLVER.py
@@ -145,7 +145,6 @@ class Solver:
                 
                 if (rt.sequenceOfNodes[j].ID != 0):
                     print(rt.sequenceOfNodes[j].ID, end=' ', )
-               
                 f.write("%d\n" % (rt.sequenceOfNodes[j].ID))
             f.write("\n")
             print(rt.sequenceOfNodes[0].ID, end=' ', )
