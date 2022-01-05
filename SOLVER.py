c58e00d7c746b03b7f49935e98e5726e4442d7ee
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2022-01-05 09:36:10 +0200

    Create SOLVER.py

---
+++ SOLVER.py
@@ -0,0 +1,175 @@
+from VRP import *
+from SolutionDrawer import *
+
+class Solution:
+    def __init__(self):
+        self.profit = 0.0
+        self.time = 0.0
+        self.routes = []
+
+class CustomerInsertion(object):
+    def __init__(self):
+        self.customer = None
+        self.route = None
+        self.profit = -(10 ** 9)
+        self.time = 10 ** 9
+        
+class CustomerInsertionAllPositions(object):
+    def __init__(self):
+        self.customer = None
+        self.route = None
+        self.insertionPosition = None
+        self.profit = -(10 ** 9)
+        self.time = 10 ** 9
+        
+        
+class RelocationMove(object):
+    def __init__(self):
+        self.originRoutePosition = None
+        self.targetRoutePosition = None
+        self.originNodePosition = None
+        self.targetNodePosition = None
+        self.distChangeOriginRt = None
+        self.distChangeTargetRt = None
+        self.timeChangeOriginRt = None
+        self.timeChangeTargetRt = None
+        self.moveCost = None
+
+    def Initialize(self):
+        self.originRoutePosition = None
+        self.targetRoutePosition = None
+        self.originNodePosition = None
+        self.targetNodePosition = None
+        self.distChangeOriginRt = None
+        self.distChangeTargetRt = None
+        self.timeChangeOriginRt = None
+        self.timeChangeTargetRt = None
+        self.moveCost = 10 ** 9
+
+class SwapMove(object):
+    def __init__(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.distChangeFirstRt = None
+        self.distChangeSecondRt = None
+        self.time1 = None
+        self.time2 = None
+        self.moveCost = None
+
+    def Initialize(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.distChangeFirstRt = None
+        self.distChangeSecondRt = None
+        self.time1 = None
+        self.time2 = None
+        self.moveCost = 10 ** 9
+
+class TwoOptMove(object):
+    def __init__(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.moveCost = None
+
+    def Initialize(self):
+        self.positionOfFirstRoute = None
+        self.positionOfSecondRoute = None
+        self.positionOfFirstNode = None
+        self.positionOfSecondNode = None
+        self.moveCost = 10 ** 9
+        
+class Solver:
+    def __init__(self, m):
+        self.allNodes = m.allNodes
+        self.depot = m.allNodes[0]
+        self.customers = m.customers
+        self.distanceMatrix = m.matrix
+        self.total_route_time = 0
+        self.total_route_profit = 0
+        self.sol = Solution()
+        self.bestSolution = Solution()
+        self.route = None
+        
+        
+     def ApplyBestNodeMethod(self):
+
+        for r in range(5):
+            self.route = Route(150)
+            node = 0
+            time_limit = 150
+            self.route.sequenceOfNodes.append(self.allNodes[0])
+            self.allNodes[0].isRouted = True
+            total_time = 0
+            total_profit = 0
+            position = 0
+            while time_limit >= 0:
+                max1 = -100000000000
+                flag = False
+                for i in range(len(self.allNodes)):
+                    if self.allNodes[i].isRouted == False:
+                        flag = True
+                        if (self.allNodes[i].profit - (self.distanceMatrix[node][i] +  self.allNodes[i].service_time)) > max1:
+                            max1 = self.allNodes[i].profit - (self.distanceMatrix[node][i] +  self.allNodes[i].service_time)
+                            position = i
+                            
+                if not flag:
+                    break
+                    
+                elif (total_route_time + (self.distanceMatrix[node][position] +  self.allNodes[position].service_time) <= 150):
+                    self.route.sequenceOfNodes.append(self.allNodes[position])
+                    self.allNodes[position].isRouted = True
+                    a = self.allNodes[position]
+                    total_profit += a.profit
+                    total_time += (self.distanceMatrix[node][position] +  a.service_time)
+                    time_limit -= (self.distanceMatrix[node][position] +  a.service_time)
+                    node = position
+                else:
+                    break
+                    
+            self.route.profit += total_profit
+            self.route.time += total_time
+            self.route.time_limit -= total_time
+            self.sol.routes.append(self.route)
+            self.sol.profit += total_profit
+
+        f = open("NearestNeighbor8180099.txt", "w+")
+        for i in range(len(self.sol.routes)):
+            rt: Route = self.sol.routes[i]
+            f.write("This is route: \n")
+            for j in range(len(rt.sequenceOfNodes)):
+                print(rt.sequenceOfNodes[j].ID, end=' ', )
+                f.write("%d\n" % (rt.sequenceOfNodes[j].ID))
+            f.write("\n")
+            print("\n")
+        solution = self.objective(self.sol)
+        f.write("This is the final objective: %d" % (solution))
+        f.close()
+        SolDrawer.draw('final_Nearest8180099', self.sol, self.allNodes)
+        return (self.sol)
+
+    ## method that calculates the total profit of the solution given
+    def objective(self, solution):
+        total_profit = 0
+        single_profit = []
+        for i in range(len(solution.routes)):
+            pr = 0
+            rout: Route = solution.routes[i]
+            for j in range(len(rout.sequenceOfNodes) - 1):
+                index1 = rout.sequenceOfNodes[j]
+                index2 = rout.sequenceOfNodes[j + 1]
+                pr += index2.profit
+            single_profit.append(pr)
+            self.sol.routes[i].profit = pr
+            total_profit += profit
+        return total_profit
+   
+        
+        
+
+        
