1c6b017220289643b56e8fb8659d763e89283e35
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2022-01-06 15:20:32 +0200

    Create MAIN_LS.py

---
+++ MAIN_LS.py
@@ -0,0 +1,10 @@
+from LOCAL_SEARCH import *
+
+# 1ο Ερώτημα
+m = Model()
+m.BuildModel()
+
+# 2ο Ερώτημα
+s = Solver(m)
+solution = s.ApplyBestNodeMethod()
+print(" TOTAL PROFIT =", s.objective(solution))
