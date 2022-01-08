e71d4ee308163dc6720393310afa4d14ddab9e06
Ioanna Papidaki <57221590+ioanna-papi@users.noreply.github.com>
2022-01-08 03:29:50 +0200

    Update MAIN_LS.py

--- MAIN_LS.py
+++ MAIN_LS.py
@@ -9,7 +9,3 @@ s = Solver(m)
 solution = s.ApplyBestNodeMethod()
 print(" TOTAL PROFIT =", s.objective(solution))
 
-print("Local Search")
-solution2 = s.LocalSearch()
-print(s.objective(solution2))
-print()
